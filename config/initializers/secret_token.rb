90b70f49c264939e4f09792adad03f10d1911b46
Jorge Moreno <jorge.moreno@glb272.(none)>
2010-12-02 16:37:32 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rhockey::Application.config.secret_token = 'dd08dd927ec8cbe03d4fd21e4885bce1a47f9f5d26f2a4b0228fafd9e9785c8d70b8972911a1c9565b6367b94bf79d06f436419ec64a00e805eae1ac289b7034'
