2f04fe9cfc2592b13e73ecbf6ea0cc6e9687798b
Jorge <jmoren@gmail.com>
2010-12-28 21:40:55 -0300

    player form fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101227182125) do
+ActiveRecord::Schema.define(:version => 20101229002914) do
 
   create_table "actions", :force => true do |t|
     t.string   "name"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(:version => 20101227182125) do
     t.boolean  "goalkeeper", :default => false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "number"
   end
 
   create_table "referis", :force => true do |t|
